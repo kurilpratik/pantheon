375a057ce691862cd9032a15c51c858ea1146107
Pratik Kuril <pratikkuril16@gmail.com>
2025-02-17 23:54:47 +0530

    Set desktop first tailwind css approach

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,13 +5,15 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <Image src={'/logo.svg'} width={150} height={150} alt='Logo'/>
-        <ul className='flex gap-4'>
-            <li>Dashboard</li>
-            <li>Search</li>
-            <li>Chat</li>
-        </ul>
+    <div className='flex justify-between p-6 px-10'>
+        <div className="flex gap-10">
+            <Image src={'/logo.svg'} width={150} height={150} alt='Logo'/>
+            <ul className='flex gap-4 items-center sm:hidden'>
+                <li>Dashboard</li>
+                <li>Search</li>
+                <li>Chat</li>
+            </ul>
+        </div>
         <div className="flex gap-2">
             <Button><Plus/>Post Your Ad</Button>
             <Button variant="outline">Login</Button>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,23 @@ export default {
   ],
   theme: {
   	extend: {
+		screens: {
+			xl: {
+				max: '1279px'
+			},
+			lg: {
+				max: '1023px'
+			},
+			md: {
+				max: '767px'
+			},
+			sm: {
+				max: '639px'
+			},
+			xs: {
+				max: '400px'
+			}
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -20,7 +37,7 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#397B26',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
